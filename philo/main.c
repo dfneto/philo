15af73466941f8b75c98f83fcf27afe6f6ec68b1
dfneto <pdavidfneto@gmail.com>
2024-02-23 16:12:40 +0100

    1 philo executing a rountine in a thread

--- philo/main.c
+++ philo/main.c
@@ -6,70 +6,124 @@
 /*   By: davifern <davifern@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/19 11:01:45 by davifern          #+#    #+#             */
-/*   Updated: 2024/02/23 13:10:01 by davifern         ###   ########.fr       */
+/*   Updated: 2024/02/23 16:12:05 by davifern         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "philo.h"
 #include <unistd.h>
+#include <stdlib.h>
+#include <pthread.h>
+
+t_god	*create_god()
+{
+	t_god	*god;
+
+	god = (t_god *)malloc(sizeof(t_god));
+	if (!god)
+		return (NULL);
+	gettimeofday(&god->start, NULL);
+	god->philo = NULL;
+	return (god);
+}
+
+t_philo	*create_philo(char **argv)
+{
+	t_philo	*philo;
+
+	philo = (t_philo *)malloc(sizeof(t_philo));
+	if (!philo)
+		return (NULL);
+	gettimeofday(&philo->fasting, NULL);
+	gettimeofday(&philo->now, NULL);
+	philo->time_to_die = ft_atoi(argv[2]);
+	philo->time_to_eat = ft_atoi(argv[3]);
+	philo->time_to_sleep = ft_atoi(argv[4]);
+	philo->n_times_eat = 0;
+	if (argv[5])
+		philo->n_times_eat = ft_atoi(argv[5]);
+	return (philo);
+}
+
+void	*routine(void *god_data)
+{
+	// faço na criação do philo
+	// gettimeofday(&start, NULL);
+	// gettimeofday(&fasting, NULL);
+
+	// usleep(3000);//simulacao para que now > fasting
+	t_philo *philo;
+	t_god	*god;
+
+	god = (t_god *)god_data;
+	philo = (t_philo *)god->philo;
+	
+	gettimeofday(&philo->now, NULL);
+	while (!philosopher_died(philo->fasting, philo->now, philo->time_to_die)) //passar philo depois
+	{
+		gettimeofday(&philo->now, NULL); //talvez possa remover as condições de morte e o gettimeofday daqui
+		printf("%.5lu X has taken a fork\n", get_current_time(god->start, philo->now));
+		if (!philosopher_died(philo->fasting, philo->now, philo->time_to_die)) //come
+		{
+			printf("%.5lu X is eating\n", get_current_time(god->start, philo->now));
+			usleep(philo->time_to_eat * 1000);
+			gettimeofday(&philo->fasting, NULL); //começa o jejum
+		}
+		
+		gettimeofday(&philo->now, NULL);
+		if (!philosopher_died(philo->fasting, philo->now, philo->time_to_die)) //dorme
+		{
+			printf("%.5lu X is sleeping\n", get_current_time(god->start, philo->now));
+			usleep(philo->time_to_sleep * 1000);
+		}
+		
+		gettimeofday(&philo->now, NULL);
+		printf("%.5lu X is thinking\n", get_current_time(god->start, philo->now)); //pensa
+	}
+	printf("%.5lu X died\n", get_current_time(god->start, philo->now)); //morre
+	
+	return (NULL);
+}
+
+void	*temp(void *data)
+{
+	(void)data;
+	printf("working\n");
+	return NULL;
+}
 
 int	main(int argc, char **argv)
 {
-	struct timeval start;
-	struct timeval now;
-	struct timeval fasting;
-
+	pthread_t	tid1;
+	
 	int	number_of_philosophers;
-	int	time_to_die;
-	int	time_to_eat;
-	int	time_to_sleep;
-	int	n_times_eat;
+	t_philo **philo_list;
+	t_philo	*philo = NULL;
+	t_god	*god;
 
 	if (check_input(argc, argv))
 		return (1);
+	
 	number_of_philosophers = ft_atoi(argv[1]);
-	time_to_die = ft_atoi(argv[2]);
-	time_to_eat = ft_atoi(argv[3]);
-	time_to_sleep = ft_atoi(argv[4]);
-	n_times_eat = 0;
-	if (argv[5])
-		n_times_eat = ft_atoi(argv[5]);
+	philo_list = (t_philo **)malloc(number_of_philosophers * sizeof(philo));
+	philo_list[0] = philo;
+
+	god = create_god();
+	philo = create_philo(argv);
+	god->philo = philo;
 	
-
-	printf("Number of philosophers: %d\n"
-		"Time to die: %d\nTime to eat: %d\nTime to sleep: %d\n"
-		"# times each philosopher must eat: %d\n", number_of_philosophers,
-		time_to_die, time_to_eat, time_to_sleep, n_times_eat);
 	
-	gettimeofday(&start, NULL);
-	gettimeofday(&fasting, NULL);
+	pthread_create(&tid1, NULL, routine, (void *)god);
+	pthread_join(tid1, NULL);
+	printf("Main: Created first thread [%ld]\n", (long)tid1);
 
-	usleep(3000);//simulacao para que now > fasting
 
-	gettimeofday(&now, NULL);
 
-	while (!philosopher_died(fasting, now, time_to_die))
-	{
-		gettimeofday(&now, NULL); //talvez possa remover as condições de morte e o gettimeofday daqui
-		printf("%.5lu X has taken a fork\n", get_current_time(start, now));
-		if (!philosopher_died(fasting, now, time_to_die)) //come
-		{
-			printf("%.5lu X is eating\n", get_current_time(start, now));
-			usleep(time_to_eat * 1000);
-			gettimeofday(&fasting, NULL); //começa o jejum
-		}
-		
-		gettimeofday(&now, NULL);
-		if (!philosopher_died(fasting, now, time_to_die)) //dorme
-		{
-			printf("%.5lu X is sleeping\n", get_current_time(start, now));
-			usleep(time_to_sleep * 1000);
-		}
-		
-		gettimeofday(&now, NULL);
-		printf("%.5lu X is thinking\n", get_current_time(start, now)); //pensa
-	}
-	printf("%.5lu X died\n", get_current_time(start, now)); //morre
+	
 
 	return (0);
 }
+	// printf("Number of philosophers: %d\n"
+	// 	"Time to die: %d\nTime to eat: %d\nTime to sleep: %d\n"
+	// 	"# times each philosopher must eat: %d\nStart time: %lu\n", number_of_philosophers,
+	// 	god->philo->time_to_die, god->philo->time_to_eat, god->philo->time_to_sleep, god->philo->n_times_eat, god->start.tv_sec);
--- philo/philo.h
+++ philo/philo.h
@@ -6,7 +6,7 @@
 /*   By: davifern <davifern@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/19 11:34:11 by davifern          #+#    #+#             */
-/*   Updated: 2024/02/23 12:59:47 by davifern         ###   ########.fr       */
+/*   Updated: 2024/02/23 16:00:52 by davifern         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,6 +15,22 @@
 
 # include <stdio.h>
 # include <sys/time.h>
+
+typedef struct s_philo
+{
+    struct timeval now;
+	struct timeval fasting;
+	int	time_to_die;
+	int	time_to_eat;
+	int	time_to_sleep;
+	int	n_times_eat;
+}   t_philo;
+
+typedef struct s_god
+{
+	struct timeval start;
+	t_philo *philo;
+}	t_god;
 
 int             ft_atoi(const char *str);
 int             check_input(int argc, char **argv);
